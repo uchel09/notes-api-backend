486627d61a7984bc5b4fc59de67e0fedf0bfa8d2
Russel <ucelrussel77@gmail.com>
2022-11-29 13:24:25 -0500

    to 4000

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import routes from './routes.js';
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 4000,
     host: 'localhost',
     routes: {
       cors: {
